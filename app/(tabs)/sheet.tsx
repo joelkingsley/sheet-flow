5f54d5aabfb77e8ec5513c9cef9082412e5d4b7a
Joel Kingsley <joelkingsley.r@gmail.com>
2025-08-14 03:04:21 +0200

    Externalized hardcoded xmls to the assets folder

--- app/(tabs)/sheet.tsx
+++ app/(tabs)/sheet.tsx
@@ -8,418 +8,48 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function SheetsScreen() {
-  const [selectedFile, setSelectedFile] = useState('1');
+  const [selectedFile, setSelectedFile] = useState('MuzioClementi_SonatinaOpus36No1_Part2.xml');
   const [showDropdown, setShowDropdown] = useState(false);
 
-// Clementi Sonatina Op.36 No.1 Part 2 - Complete MusicXML
-const clementniXML = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 2.0 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
-<score-partwise>
-  <work>
-    <work-title>Sonatina Op.36 No 1 Teil 2 Andante</work-title>
-    </work>
-  <identification>
-  <creator type="composer">Muzio Clementi</creator>
-    <encoding>
-      <software>MuseScore 1.2</software>
-      <encoding-date>2012-08-06</encoding-date>
-      </encoding>
-    </identification>
-  <defaults>
-    <scaling>
-      <millimeters>8.46668</millimeters>
-      <tenths>40</tenths>
-      </scaling>
-    <page-layout>
-      <page-height>1320</page-height>
-      <page-width>1020</page-width>
-      <page-margins type="both">
-        <left-margin>59.5832</left-margin>
-        <right-margin>56.6667</right-margin>
-        <top-margin>96.6665</top-margin>
-        <bottom-margin>79.9999</bottom-margin>
-        </page-margins>
-      </page-layout>
-    </defaults>
-  <credit page="1">
-    <credit-words default-x="509.999" default-y="1223.33" font-size="24" justify="center" valign="top">Sonatina op36 No1 Part2</credit-words>
-    </credit>
-  <credit page="1">
-    <credit-words default-x="963.332" default-y="1150.83" font-size="12" justify="right" valign="top">Muzio Clementi</credit-words>
-    </credit>
-  <part-list>
-    <score-part id="P1">
-      <part-name>Piano (right)</part-name>
-      <score-instrument id="P1-I3">
-        <instrument-name>Piano (right)</instrument-name>
-        </score-instrument>
-      <midi-instrument id="P1-I3">
-        <midi-channel>1</midi-channel>
-        <midi-program>1</midi-program>
-        <volume>78.7402</volume>
-        <pan>0</pan>
-        </midi-instrument>
-      </score-part>
-    <score-part id="P2">
-      <part-name>Piano (left)</part-name>
-      <score-instrument id="P2-I3">
-        <instrument-name>Piano (left)</instrument-name>
-        </score-instrument>
-      <midi-instrument id="P2-I3">
-        <midi-channel>2</midi-channel>
-        <midi-program>1</midi-program>
-        <volume>78.7402</volume>
-        <pan>0</pan>
-        </midi-instrument>
-      </score-part>
-    </part-list>
-  <part id="P1">
-    <measure number="1" width="308.00">
-      <print>
-        <system-layout>
-          <system-margins>
-            <left-margin>120.31</left-margin>
-            <right-margin>0.00</right-margin>
-            </system-margins>
-          <top-system-distance>271.11</top-system-distance>
-          </system-layout>
-        </print>
-      <attributes>
-        <divisions>12</divisions>
-        <key>
-          <fifths>-1</fifths>
-          <mode>major</mode>
-          </key>
-        <time>
-          <beats>3</beats>
-          <beat-type>4</beat-type>
-          </time>
-        <clef>
-          <sign>G</sign>
-          <line>2</line>
-          </clef>
-        </attributes>
-      <direction placement="above">
-        <direction-type>
-          <metronome parentheses="no">
-            <beat-unit>quarter</beat-unit>
-            <per-minute>92</per-minute>
-            </metronome>
-          </direction-type>
-        <sound tempo="91.9998"/>
-        </direction>
-      <direction placement="above">
-        <direction-type>
-          <words>Andante</words>
-          </direction-type>
-        </direction>
-      <note default-x="82.28" default-y="-15.00">
-        <pitch>
-          <step>C</step>
-          <octave>5</octave>
-          </pitch>
-        <duration>24</duration>
-        <voice>1</voice>
-        <type>half</type>
-        <stem>down</stem>
-        </note>
-      <note default-x="231.70" default-y="0.00">
-        <pitch>
-          <step>F</step>
-          <octave>5</octave>
-          </pitch>
-        <duration>12</duration>
-        <voice>1</voice>
-        <type>quarter</type>
-        <stem>down</stem>
-        </note>
-      </measure>
-    </part>
-    <part id="P2">
-      <measure number="1" width="308.00">
-        <print>
-          <staff-layout number="1">
-            <staff-distance>65.00</staff-distance>
-            </staff-layout>
-          </print>
-        <attributes>
-          <divisions>12</divisions>
-          <key>
-            <fifths>-1</fifths>
-            <mode>major</mode>
-            </key>
-          <time>
-            <beats>3</beats>
-            <beat-type>4</beat-type>
-            </time>
-          <clef>
-            <sign>F</sign>
-            <line>4</line>
-            </clef>
-          </attributes>
-        <note default-x="82.28" default-y="-115.00">
-          <pitch>
-            <step>F</step>
-            <octave>3</octave>
-            </pitch>
-          <duration>4</duration>
-          <voice>1</voice>
-          <type>eighth</type>
-          <time-modification>
-            <actual-notes>3</actual-notes>
-            <normal-notes>2</normal-notes>
-            </time-modification>
-          <stem>down</stem>
-          <beam number="1">begin</beam>
-          <notations>
-            <tuplet type="start" bracket="no"/>
-            </notations>
-          </note>
-        </measure>
-      </part>
-  </score-partwise>`;
+  // List of available MusicXML files in assets/sheets
+  const musicFiles = [
+    {
+      id: 'MuzioClementi_SonatinaOpus36No1_Part2.xml',
+      title: 'Sonatina Op.36 No.1 - Andante',
+      composer: 'Muzio Clementi',
+      path: require('@/assets/sheets/MuzioClementi_SonatinaOpus36No1_Part2.xml'),
+    },
+    {
+      id: 'Beethoven_AnDieFerneGeliebte.xml',
+      title: 'An die ferne Geliebte - Op. 98',
+      composer: 'Ludwig van Beethoven',
+      path: require('@/assets/sheets/Beethoven_AnDieFerneGeliebte.xml'),
+    },
+  ];
 
-// Beethoven An die ferne Geliebte - First page
-const beethovenXML = `<?xml version="1.0" encoding="UTF-8"?>
-<score-partwise version="2.0">
-  <work>
-    <work-number>Op. 98</work-number>
-    <work-title>An die ferne Geliebte (Page 1)</work-title>
-  </work>
-  <identification>
-    <creator type="composer">Ludwig van Beethoven</creator>
-    <creator type="lyricist">Aloys Jeitteles</creator>
-    <rights>Copyright © 2002 Recordare LLC</rights>
-    <encoding>
-      <software>Finale 2011 for Windows</software>
-      <software>Dolet Light for Finale 2011</software>
-      <encoding-date>2010-12-10</encoding-date>
-      <supports attribute="new-system" element="print" type="yes" value="yes"/>
-      <supports attribute="new-page" element="print" type="yes" value="yes"/>
-    </encoding>
-  </identification>
-  <defaults>
-    <scaling>
-      <millimeters>6.35</millimeters>
-      <tenths>40</tenths>
-    </scaling>
-    <page-layout>
-      <page-height>1760</page-height>
-      <page-width>1360</page-width>
-      <page-margins type="both">
-        <left-margin>80</left-margin>
-        <right-margin>80</right-margin>
-        <top-margin>80</top-margin>
-        <bottom-margin>80</bottom-margin>
-      </page-margins>
-    </page-layout>
-    <system-layout>
-      <system-margins>
-        <left-margin>71</left-margin>
-        <right-margin>0</right-margin>
-      </system-margins>
-      <system-distance>108</system-distance>
-      <top-system-distance>65</top-system-distance>
-    </system-layout>
-    <staff-layout>
-      <staff-distance>101</staff-distance>
-    </staff-layout>
-    <appearance>
-      <line-width type="stem">0.957</line-width>
-      <line-width type="beam">5.0391</line-width>
-      <line-width type="staff">0.957</line-width>
-      <line-width type="light barline">1.875</line-width>
-      <line-width type="heavy barline">5.0391</line-width>
-      <line-width type="leger">1.875</line-width>
-      <line-width type="ending">0.957</line-width>
-      <line-width type="wedge">0.957</line-width>
-      <line-width type="enclosure">0.957</line-width>
-      <line-width type="tuplet bracket">0.957</line-width>
-      <note-size type="grace">60</note-size>
-      <note-size type="cue">60</note-size>
-    </appearance>
-    <music-font font-family="Maestro" font-size="18"/>
-    <word-font font-family="Times New Roman" font-size="9"/>
-    <lyric-font font-family="Times New Roman" font-size="10"/>
-  </defaults>
-  <credit page="1">
-    <credit-words default-x="680" default-y="1678" font-size="24" justify="center" valign="top">An die ferne Geliebte</credit-words>
-    <credit-words font-size="24" font-weight="bold"> 
-</credit-words>
-    <credit-words font-size="14" font-weight="normal">Op. 98</credit-words>
-  </credit>
-  <part-list>
-    <score-part id="P1">
-      <part-name>Voice</part-name>
-      <score-instrument id="P1-I3">
-        <instrument-name>Voice</instrument-name>
-      </score-instrument>
-      <midi-instrument id="P1-I3">
-        <midi-channel>1</midi-channel>
-        <midi-program>53</midi-program>
-        <volume>80</volume>
-        <pan>0</pan>
-      </midi-instrument>
-    </score-part>
-    <score-part id="P2">
-      <part-name>Piano</part-name>
-      <score-instrument id="P2-I2">
-        <instrument-name>Acoustic Grand Piano</instrument-name>
-      </score-instrument>
-      <midi-instrument id="P2-I2">
-        <midi-channel>2</midi-channel>
-        <midi-program>1</midi-program>
-        <volume>80</volume>
-        <pan>0</pan>
-      </midi-instrument>
-    </score-part>
-  </part-list>
-  <part id="P1">
-    <measure number="1" width="324">
-      <print page-number="1">
-        <system-layout>
-          <system-margins>
-            <left-margin>150</left-margin>
-            <right-margin>0</right-margin>
-          </system-margins>
-          <top-system-distance>300</top-system-distance>
-        </system-layout>
-        <measure-numbering>system</measure-numbering>
-      </print>
-      <attributes>
-        <divisions>24</divisions>
-        <key>
-          <fifths>-3</fifths>
-          <mode>major</mode>
-        </key>
-        <time>
-          <beats>3</beats>
-          <beat-type>4</beat-type>
-        </time>
-        <clef>
-          <sign>G</sign>
-          <line>2</line>
-        </clef>
-      </attributes>
-      <direction directive="yes" placement="above">
-        <direction-type>
-          <words default-y="26" font-size="11" font-weight="bold">Ziemlich langsam und mit Ausdruck</words>
-        </direction-type>
-        <sound tempo="60"/>
-      </direction>
-      <direction directive="yes" placement="above">
-        <direction-type>
-          <words default-y="60" font-size="16" font-weight="bold" xml:lang="de">No. 1</words>
-        </direction-type>
-      </direction>
-      <note default-x="136">
-        <rest/>
-        <duration>24</duration>
-        <voice>1</voice>
-        <type>quarter</type>
-      </note>
-      <note default-x="196">
-        <pitch>
-          <step>B</step>
-          <alter>-1</alter>
-          <octave>4</octave>
-        </pitch>
-        <duration>24</duration>
-        <voice>1</voice>
-        <type>quarter</type>
-        <stem default-y="-55.5">down</stem>
-        <lyric default-y="-80" number="1">
-          <syllabic>single</syllabic>
-          <text>Auf</text>
-        </lyric>
-      </note>
-      <note default-x="257">
-        <pitch>
-          <step>B</step>
-          <alter>-1</alter>
-          <octave>4</octave>
-        </pitch>
-        <duration>24</duration>
-        <voice>1</voice>
-        <type>quarter</type>
-        <stem default-y="-55.5">down</stem>
-        <lyric default-y="-80" number="1">
-          <syllabic>single</syllabic>
-          <text>dem</text>
-        </lyric>
-      </note>
-    </measure>
-  </part>
-  <part id="P2">
-    <measure number="1" width="324">
-      <print>
-        <staff-layout number="2">
-          <staff-distance>70</staff-distance>
-        </staff-layout>
-        <measure-numbering>none</measure-numbering>
-      </print>
-      <attributes>
-        <divisions>96</divisions>
-        <key>
-          <fifths>-3</fifths>
-          <mode>major</mode>
-        </key>
-        <time>
-          <beats>3</beats>
-          <beat-type>4</beat-type>
-        </time>
-        <staves>2</staves>
-        <clef number="1">
-          <sign>G</sign>
-          <line>2</line>
-        </clef>
-        <clef number="2">
-          <sign>F</sign>
-          <line>4</line>
-        </clef>
-      </attributes>
-      <direction placement="below">
-        <direction-type>
-          <dynamics default-y="-81">
-            <p/>
-          </dynamics>
-        </direction-type>
-        <staff>1</staff>
-        <sound dynamics="54"/>
-      </direction>
-      <note default-x="136">
-        <pitch>
-          <step>B</step>
-          <alter>-1</alter>
-          <octave>3</octave>
-        </pitch>
-        <duration>96</duration>
-        <voice>1</voice>
-        <type>quarter</type>
-        <stem default-y="15.5">up</stem>
-        <staff>1</staff>
-      </note>
-    </measure>
-  </part>
-</score-partwise>`;
+  // State to hold loaded XML content
+  const [musicXML, setMusicXML] = useState<string>('');
 
-const musicFiles = [
-  {
-    id: '1',
-    title: 'Sonatina Op.36 No.1 - Andante',
-    composer: 'Muzio Clementi',
-    xml: clementniXML
-  },
-  {
-    id: '2',
-    title: 'An die ferne Geliebte - Op. 98',
-    composer: 'Ludwig van Beethoven',
-    xml: beethovenXML
-  }
-];  const getMusicXML = () => {
-    const selected = musicFiles.find(file => file.id === selectedFile);
-    return selected ? selected.xml : musicFiles[0].xml;
-  };
+  // Load XML when selectedFile changes
+  React.useEffect(() => {
+    const loadXML = async () => {
+      const selected = musicFiles.find(file => file.id === selectedFile);
+      if (!selected) {
+        setMusicXML('');
+        return;
+      }
+      if (Platform.OS === 'web') {
+        const response = await fetch(`/assets/sheets/${selected.id}`);
+        const text = await response.text();
+        setMusicXML(text);
+      } else {
+        // For native, you may need to use expo-file-system or asset loader
+        // Here, we just set the asset reference (SheetMusicDisplay should handle it)
+        setMusicXML(selected.path);
+      }
+    };
+    loadXML();
+  }, [selectedFile]);
 
   const showPicker = () => {
     if (Platform.OS === 'web') {
@@ -497,10 +127,9 @@ const musicFiles = [
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Sheet Music Display</ThemedText>
         <ThemedText>Selected: {getSelectedLabel()}</ThemedText>
-        
         {/* Sheet music display */}
         <SheetMusicDisplay 
-          musicXML={getMusicXML()} 
+          musicXML={musicXML} 
           style={styles.sheetMusicContainer}
         />
       </ThemedView>
--- components/SheetMusicDisplay.native.tsx
+++ components/SheetMusicDisplay.native.tsx
@@ -1,114 +1,155 @@
-import React from 'react';
+import { Asset } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { ThemedText } from './ThemedText';
 
 interface SheetMusicDisplayNativeProps {
-  musicXML: string;
+  musicXML: string | any;
   style?: any;
 }
 
 const SheetMusicDisplayNative: React.FC<SheetMusicDisplayNativeProps> = ({ musicXML, style }) => {
+  const [xmlString, setXmlString] = useState<string>('');
+
+  useEffect(() => {
+    let isMounted = true;
+    const loadXML = async () => {
+      if (typeof musicXML === 'string') {
+        setXmlString(musicXML);
+      } else if (musicXML) {
+        // Use expo-asset to resolve asset URI
+        try {
+          const asset = Asset.fromModule(musicXML);
+          await asset.downloadAsync();
+          const fileContent = await FileSystem.readAsStringAsync(asset.localUri || asset.uri);
+          if (isMounted) setXmlString(fileContent);
+        } catch (err) {
+          if (isMounted) setXmlString('');
+        }
+      } else {
+        setXmlString('');
+      }
+    };
+    loadXML();
+    return () => { isMounted = false; };
+  }, [musicXML]);
+
   const generateWebViewHTML = () => {
-    return `
+  // Use JSON.stringify for safe JS injection
+  const safeXML = JSON.stringify(xmlString || '');
+  let html = `
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OpenSheetMusicDisplay</title>
-    <script src="https://unpkg.com/opensheetmusicdisplay@1.8.7/build/opensheetmusicdisplay.min.js"></script>
-    <style>
-        body {
-            margin: 0;
-            padding: 20px;
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-        }
-        #osmdContainer {
-            width: 100%;
-            height: auto;
-            min-height: 400px;
-            background-color: white;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-            padding: 20px;
-            box-sizing: border-box;
-        }
-        .loading {
-            text-align: center;
-            padding: 40px;
-            color: #666;
-        }
-        .error {
-            text-align: center;
-            padding: 40px;
-            color: #d32f2f;
-            background-color: #ffebee;
-            border-radius: 4px;
-            margin: 20px 0;
-        }
-    </style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OpenSheetMusicDisplay</title>
+  <script src="https://unpkg.com/opensheetmusicdisplay@1.8.7/build/opensheetmusicdisplay.min.js"></script>
+  <style>
+    body {
+      margin: 0;
+      padding: 20px;
+      font-family: Arial, sans-serif;
+      background-color: #f5f5f5;
+    }
+    #osmdContainer {
+      width: 100%;
+      height: auto;
+      min-height: 400px;
+      background-color: white;
+      border-radius: 8px;
+      box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    .loading {
+      text-align: center;
+      padding: 40px;
+      color: #666;
+    }
+    .error {
+      text-align: center;
+      padding: 40px;
+      color: #d32f2f;
+      background-color: #ffebee;
+      border-radius: 4px;
+      margin: 20px 0;
+    }
+  </style>
 </head>
 <body>
-    <div id="osmdContainer">
-        <div class="loading">Loading sheet music...</div>
-    </div>
+  <div id="osmdContainer">
+    <div class="loading">Loading sheet music...</div>
+  </div>
     
-    <script>
-        function loadSheetMusic() {
-            try {
-                const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdContainer");
-                
-                const musicXML = \`${musicXML.replace(/`/g, '\\`')}\`;
-                
-                osmd.load(musicXML).then(() => {
-                    osmd.render();
-                    console.log("Sheet music rendered successfully");
-                }).catch((error) => {
-                    console.error("Error rendering sheet music:", error);
-                    document.getElementById("osmdContainer").innerHTML = 
-                        '<div class="error">Error loading sheet music: ' + error.message + '</div>';
-                });
-            } catch (error) {
-                console.error("Error initializing OSMD:", error);
-                document.getElementById("osmdContainer").innerHTML = 
-                    '<div class="error">Error initializing sheet music display: ' + error.message + '</div>';
-            }
-        }
-        
-        // Load when the page is ready
-        if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', loadSheetMusic);
-        } else {
-            loadSheetMusic();
-        }
-    </script>
+  <script>
+    function loadSheetMusic() {
+      try {
+        const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdContainer");
+  const musicXML = REPLACE_MUSICXML;
+        osmd.load(musicXML).then(() => {
+          osmd.render();
+          document.getElementById("osmdContainer").innerHTML = ""; // Remove loading text
+          console.log("Sheet music rendered successfully");
+        }).catch((error) => {
+          console.error("Error rendering sheet music:", error);
+          document.getElementById("osmdContainer").innerHTML = 
+            '<div class="error">Error loading sheet music: ' + error.message + '<br><pre>' + (error.stack || '') + '</pre></div>';
+        });
+      } catch (error) {
+        console.error("Error initializing OSMD:", error);
+        document.getElementById("osmdContainer").innerHTML = 
+          '<div class="error">Error initializing sheet music display: ' + error.message + '</div>';
+      }
+    }
+    // Load when the page is ready
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', loadSheetMusic);
+    } else {
+      loadSheetMusic();
+    }
+  </script>
 </body>
 </html>`;
+  html = html.replace('REPLACE_MUSICXML', safeXML);
+  // safeXML is JSON.stringify(xmlString), so this injects a valid JS string literal
+  return html;
   };
+
+  // Debug: log the XML string before rendering
+  React.useEffect(() => {
+    console.log('SheetMusicDisplayNative xmlString:', xmlString);
+  }, [xmlString]);
 
   return (
     <View style={[styles.container, style]}>
-      <WebView
-        source={{ html: generateWebViewHTML() }}
-        style={styles.webView}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        renderLoading={() => (
-          <View style={styles.loadingContainer}>
-            <ThemedText style={styles.loadingText}>Loading sheet music...</ThemedText>
-          </View>
-        )}
-        onError={(syntheticEvent) => {
-          const { nativeEvent } = syntheticEvent;
-          console.error('WebView error: ', nativeEvent);
-        }}
-        onMessage={(event) => {
-          console.log('WebView message:', event.nativeEvent.data);
-        }}
-      />
+      {xmlString ? (
+        <WebView
+          source={{ html: generateWebViewHTML() }}
+          style={styles.webView}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <View style={styles.loadingContainer}>
+              <ThemedText style={styles.loadingText}>Loading sheet music...</ThemedText>
+            </View>
+          )}
+          onError={(syntheticEvent) => {
+            const { nativeEvent } = syntheticEvent;
+            console.error('WebView error: ', nativeEvent);
+          }}
+          onMessage={(event) => {
+            console.log('WebView message:', event.nativeEvent.data);
+          }}
+        />
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ThemedText style={styles.loadingText}>Loading sheet music...</ThemedText>
+        </View>
+      )}
     </View>
   );
 };
